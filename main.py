2e36c8a19fe5136bc85931ddfafc8ae5f16b61a6
Chandu Surisetty <68140710+chandusurisetty@users.noreply.github.com>
2023-06-05 21:53:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,39 +5,49 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 board=[]
 previos=[]
 text="Suduko"
-@app.route("/")
-
+@app.route("/",methods=["POST","GET"])
 def home():
-    
-    return render_template("index.html",text=text, board=board,previos=previos)
-
-@app.route("/solve",methods=["POST"])
-def solve():
-    sudoarray=[]
-    global text
-    if request.method=="POST":
-        
+    if request.method=="GET":
+        return render_template("index.html",text="Suduko", board=[])    
+    else:
+        sudukoarray=[]
         for i in range(9):
             dd=[]
-            
             for j in range(9):
-
                 dd.append(request.form[f"{i}{j}"])
+            sudukoarray.append(dd)
+        suduko=Solve(sudukoarray)
+        if suduko.solve():
+            board=suduko.board
+            return render_template("index.html",text="solved",board=board)
+        return render_template("index.html",text="failed",board=sudukoarray)
+# @app.route("/solve",methods=["POST"])
+# def solve():
+#     sudoarray=[]
+#     global text
+#     if request.method=="POST":
+        
+#         for i in range(9):
+#             dd=[]
+            
+#             for j in range(9):
+
+#                 dd.append(request.form[f"{i}{j}"])
                 
-            sudoarray.append(dd)
-    global previos
-    previos= sudoarray
-    #error is hear
-    sudoko=Solve(sudoarray)
-    if sudoko.solve():
-        global board
-        board=sudoko.board
-        text="Suduko"
-        return redirect(url_for("home"))
-    board=previos
+#             sudoarray.append(dd)
+#     global previos
+#     previos= sudoarray
+#     #error is hear
+#     sudoko=Solve(sudoarray)
+#     if sudoko.solve():
+#         global board
+#         board=sudoko.board
+#         text="Suduko"
+#         return redirect(url_for("home"))
+#     board=previos
     
-    text="fail"
-    return redirect(url_for("home"))
+#     text="fail"
+#     return redirect(url_for("home"))
 
 
 if __name__=="__main__":
